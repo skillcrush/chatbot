cea5a56aa461ca7ebf8d8bbf7db5484f03a91493
lsavoie <twosavoie@gmail.com>
2024-03-24 18:11:36 -0500

    Initial commit

--- chatbot.py
+++ chatbot.py
@@ -2,6 +2,23 @@ from openai import OpenAI
 
 client = OpenAI()
 
+# accepts a preferred model and a list of messages
+# makes chat completions API call
+# returns the response message content
+def get_api_chat_response_message(model, messages):
+    # make the API call
+    api_response = client.chat.completions.create(
+        model = model,
+        messages = messages
+    )
+
+    # extract the response text
+    response_content = api_response.choices[0].message.content
+
+    # return the response text
+    return response_content
+
+# prompt the user to ask a question
 user_input = input("\nAsk something...\n\n")
 
 model = "gpt-3.5-turbo"
@@ -11,12 +28,6 @@ messages = [
     {"role": "user", "content": user_input}
 ]
 
-response = client.chat.completions.create(
-    model = model,
-    messages = messages
-)
-response_for_user = response.choices[0].message.content
+response_for_user = get_api_chat_response_message(model, messages)
 
 print("\n" + response_for_user + "\n")
-
-
